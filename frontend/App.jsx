3db230fe72c8d707b08ea24f1c6036241cd628b3
Nitin Issac Joy <nitin.i.joy@gmail.com>
2017-09-16 19:59:07 +0530

    Adding React.js

---
+++ frontend/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+          <div>
+         <h>First React App </h>
+         <div>
+           {1+2}
+         </div>
+         </div>
+         
+      );
+   }
+}
+
+export default App;
